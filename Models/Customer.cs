ccd49f19502848571b41b44e71ead8cf58936e04
sonusathyadas <sonusathyadas@hotmail.com>
2025-09-25 16:39:13 +0530

    Add Customer model with properties for Id, Name, Email, Mobile, and Address

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+namespace BookWebApi.Models
+{
+	// Customer model with id, name, email, mobile and address
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Mobile { get; set; }
+		public string Address { get; set; }
+	}
+}
